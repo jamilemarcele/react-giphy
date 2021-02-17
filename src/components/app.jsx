765e43c617e98ee0ddd9ee80070bc1ce3ccc1248
Jamile Radloff <jamilemarcele@gmail.com>
2021-02-17 19:42:45 +0100

    giphy api working

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,18 +1,44 @@
 import React, { Component } from "react";
 import SearchBar from "./search_bar";
 import Gif from "./gif";
+import GifList from "./gif_list";
+import giphy from 'giphy-api';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      gifs: [],
+      selectedGifId: "dUTObKnNrLa9KeCjDi"
+    }
+  }
+
+  search = (query) => {
+    giphy('cNa8lVK7oG476N1WotcGaiyEONJqFQhF')
+      .search({
+        q: query,
+        rating: 'g',
+        limit: 10
+      }, (error, result) => {
+        this.setState({
+          gifs: result.data
+        });
+      });
+  }
+
   render() {
     return (
       <div>
         <div className="left-scene">
-          <SearchBar />
+          <SearchBar searchFunction={this.search} />
           <div className="selected-gif">
-            <Gif />
+            <Gif id={this.state.selectedGifId} />
           </div>
         </div>
-        <div className="right-scene" />
+        <div className="right-scene">
+          <GifList gifs={this.state.gifs}/>
+        </div>
       </div>
     );
   }
--- src/components/gif.jsx
+++ src/components/gif.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class Gif extends Component {
   render() {
-    const src = "https://media3.giphy.com/media/dUTObKnNrLa9KeCjDi/giphy.gif?cid=ecf05e479fb6453e699d56fd8948b0de406a1d077d92514f&rid=giphy.gif"
+    const src = `https://media.giphy.com/media/${this.props.id}/giphy.gif`;
     return (
       <img src={src} alt="" className="gif" />
     );
